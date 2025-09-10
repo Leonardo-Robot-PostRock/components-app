ae623f1809aab7343b3dfea4601a9ddc4fd729eb
Leonardo <leonardo38361@gmail.com>
2025-09-10 14:57:12 -0300

    paleta de colores y componente View customizable

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,18 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import '../global.css';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useThemeColor } from '@/hooks/useThemeColor';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { Text } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
+  const backgroundColor = useThemeColor({}, 'background');
+
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -18,12 +24,17 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ backgroundColor: backgroundColor, flex: 1 }}>
+
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <ThemedView margin>
+          <Text className='mt-10 text-3xl text-light-text dark:text-dark-text'>Hola Mundo</Text>
+        </ThemedView>
+        {/* <Stack>
+
+</Stack> */}
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
---
+++ presentation/shared/ThemedView.tsx
@@ -0,0 +1,36 @@
+import { useThemeColor } from '@/hooks/useThemeColor';
+import { View, ViewProps } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+interface Props extends ViewProps {
+    className?: 'string';
+    margin?: boolean;
+    safe?: boolean;
+    bgColor?: string;
+}
+
+const ThemedView = ({ style, className, margin = false, safe = false, bgColor, children }: Props) => {
+
+    const backgroundColor = bgColor ?? useThemeColor({}, 'background')
+    const safeArea = useSafeAreaInsets()
+
+    // Distintas maneras de crear un componente custom
+
+    // Recomendable no usar style y className; decidir por uno de los 2
+
+    // className='bg-light-background dark:bg-dark-background'
+
+    return (
+        <View style={[
+            {
+                backgroundColor: backgroundColor,
+                flex: 1,
+                paddingTop: safe ? safeArea.top : 0,
+                marginHorizontal: margin ? 10 : 0
+            }, style
+        ]}
+        className={className}
+        >{children}</View>)
+}
+
+export default ThemedView
