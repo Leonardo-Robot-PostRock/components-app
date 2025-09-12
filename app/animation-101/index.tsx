9edd7d7c8a9306e7ffc0ee25e9a772276aca7faa
Leonardo <leonardo38361@gmail.com>
2025-09-12 18:22:47 -0300

    animaciones drag an drop y otras

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,26 +1,10 @@
+import { useAnimation } from '@/hooks/useAnimation';
 import ThemedButton from '@/presentation/shared/ThemedButton';
 import ThemedView from '@/presentation/shared/ThemedView';
-import { useRef } from 'react';
-import { Animated } from 'react-native';
+import { Animated, Easing } from 'react-native';
 
 const Animation101Screen = () => {
-  const animatedOpacity = useRef(new Animated.Value(0)).current
-
-  const fadeIn = () => {
-    Animated.timing(animatedOpacity, {
-      toValue: 1,
-      duration: 300,
-      useNativeDriver: true
-    }).start()
-  }
-
-  const fadeOut = () => {
-    Animated.timing(animatedOpacity, {
-      toValue: 0,
-      duration: 300,
-      useNativeDriver: true
-    }).start()
-  }
+  const { animatedOpacity, animatedTop, fadeIn, fadeOut, startMovingTopPosition } = useAnimation()
 
   return (
     <ThemedView margin className='justify-center items-center flex-1'>
@@ -29,15 +13,22 @@ const Animation101Screen = () => {
         style={{
           width: 150,
           height: 150,
-          opacity: animatedOpacity
+          opacity: animatedOpacity,
+          transform: [{ translateY: animatedTop }]
         }}
       />
 
-      <ThemedButton onPress={fadeIn} className='mt-10 mb-2'>
+      <ThemedButton
+        onPress={() => {
+          fadeIn({});
+          startMovingTopPosition({ easing: Easing.bounce, duration: 700 })
+        }}
+        className='mt-10 mb-2'
+      >
         FadeIn
       </ThemedButton>
 
-      <ThemedButton onPress={fadeOut} className='my-2'>
+      <ThemedButton onPress={() => fadeOut({})} className='my-2'>
         FadeIn
       </ThemedButton>
     </ThemedView>
