64d09b42f8f76382c51ee599ef62811703483769
Leonardo <leonardo38361@gmail.com>
2025-09-15 11:45:47 -0300

    switch y código por plataforma

---
+++ presentation/shared/ThemedSwitch.tsx
@@ -0,0 +1,35 @@
+import { useThemeColor } from '@/hooks/useThemeColor';
+import { Platform, Pressable, Switch, View } from 'react-native';
+import ThemedText from './ThemedText';
+
+interface Props {
+    text?: string;
+    value: boolean;
+    className?: string;
+    onValueChange: (value: boolean) => void;
+}
+
+const isAndroid = Platform.OS === 'android';
+
+const ThemedSwitch = ({ text, value, className, onValueChange }: Props) => {
+    const switchActiveColor = useThemeColor({}, 'primary')
+    return (
+        <Pressable className={`flex flex-row items-center justify-between active:opacity-80 ${className}`}
+            onPress={() => onValueChange(!value)}
+        >
+            {text ? <ThemedText type='h2'>{text}</ThemedText> : <View />}
+            <Switch
+                value={value}
+                onValueChange={onValueChange}
+                thumbColor={isAndroid ? switchActiveColor : ''}
+                trackColor={{
+                    false: 'gray',
+                    true: switchActiveColor
+                }}
+            // ios_backgroundColor={value ? 'green' : 'red'}
+            />
+        </Pressable>
+    )
+}
+
+export default ThemedSwitch
