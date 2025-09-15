ee6bc9f047ad0201f02736d155887a906ce952d7
Leonardo <leonardo38361@gmail.com>
2025-09-15 12:32:25 -0300

    alertas

--- presentation/shared/ThemedButton.tsx
+++ presentation/shared/ThemedButton.tsx
@@ -3,14 +3,15 @@ import ThemedText from './ThemedText';
 
 interface Props extends PressableProps {
     className?: string;
+    children: string;
 }
 
-const ThemedButton = ({ className, ...rest }: Props) => {
+const ThemedButton = ({ className, children, ...rest }: Props) => {
     return (
         <Pressable
             {...rest}
             className={`bg-light-primary text-light-primary dark:text-dark-primary dark:bg-dark-tertiary items-center rounded-xl px-6 py-2 active:opacity-80 ${className}`}>
-            <ThemedText className='text-white'>ThemedButton</ThemedText>
+            <ThemedText className='text-white'>{children}</ThemedText>
         </Pressable>
     )
 }
