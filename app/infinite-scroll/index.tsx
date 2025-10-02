2290eb61146dac607e4fe88c28163bab1c783b09
Leonardo <leonardo38361@gmail.com>
2025-10-02 17:28:21 -0300

    scroll infinito

--- app/infinite-scroll/index.tsx
+++ app/infinite-scroll/index.tsx
@@ -1,10 +1,47 @@
-import { View, Text } from 'react-native';
+import ThemedText from '@/presentation/shared/ThemedText';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { useState } from 'react';
+import { Image } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
 const InfiniteScrollScreen = () => {
+  const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5])
+
+  const loadMore = () => {
+    const newArray = Array.from({ length: 5 }, (_, i) => numbers.length + i)
+
+    setTimeout(() => {
+      setNumbers([...numbers, ...newArray])
+    }, 3000)
+  }
+
   return (
-    <View>
-      <Text>InfiniteScrollScreen</Text>
-    </View>
+    <ThemedView>
+
+      <FlatList
+        data={numbers}
+        renderItem={({ item }) => <ListItems number={item} />}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.6}
+      />
+
+    </ThemedView>
   );
 };
 export default InfiniteScrollScreen;
+
+interface ListItemProps {
+  number: number
+}
+
+const ListItems = ({ number }: ListItemProps) => {
+  return (
+    <Image
+      source={{ uri: `https://picsum.photos/id/${number}/500/400` }}
+      style={{
+        height: 400,
+        width: '100%'
+      }}
+    />
+  )
+}
